4877c0b55c5d4a6ffde4afce260ab2bf219aeaf3
soloman.err <soloman.err@gmail.com>
2023-05-12 15:18:13 +0600

    updated post card & added react icons

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { FaHeart, FaLinkedin, FaThumbsUp } from "react-icons/fa";
 
 const Post = () => {
   return (
-    <div className="border rounded p-1">
-      <div className="flex flex-col p-2">
+    <div className="border rounded p-2">
+      <div className="flex flex-col">
         <div className="flex gap-2 border-b border-zinc-600">
           <img
             className="w-10 h-10 object-cover rounded-full"
@@ -12,7 +13,7 @@ const Post = () => {
           />
           <div className="">
             <h3>Ell On Mask</h3>
-            <small>May4 2023</small>
+            <small>04 minutes ago</small>
           </div>
         </div>
         <div>
@@ -24,8 +25,16 @@ const Post = () => {
           </p>
         </div>
       </div>
-      <div>
-        <p>comment</p>
+      <div className="my-2">
+        <div className="text-xs space-x-2">
+          <span className="bg-zinc-800 p-1 rounded-xl">JavaScript</span>
+          <span className="bg-zinc-800 p-1 rounded-xl">NextJs</span>
+          <span className="bg-zinc-800 p-1 rounded-xl">Laravel</span>
+        </div>
+        <div className="mt-5 flex gap-2 text-sm items-center">
+          <FaThumbsUp className="text-[#6ab4cf]" />
+          <span>5likes</span>
+        </div>
       </div>
     </div>
   );
